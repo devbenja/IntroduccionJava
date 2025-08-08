a9e7486de6ea8bd07a19a99e2403684ada6153e5
devbenja <cariasbenjamin59@gmail.com>
2025-08-08 15:55:31 -0600

    Constantes, Operadores

---
+++ src/Constantes.java
@@ -0,0 +1,9 @@
+public class Constantes {
+    public static final double PI = 3.1416;
+    public static final int EDAD_MINIMA = 18;
+
+    public static void main(String[] args) {
+        System.out.println("Valor de PI: " + PI);
+        System.out.println("Edad mínima permitida: " + EDAD_MINIMA);
+    }
+}
---
+++ src/Operadores.java
@@ -0,0 +1,27 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numeroUno =  8;
+        int numeroDos = 4;
+
+        // OPERADOR DE SUMA
+        int suma = numeroUno + numeroDos;
+
+        //OPERADOR DE RESTA
+        int resto = numeroUno - numeroDos;
+
+        //OPERADOR DE MULTIPLICACION
+        int multiplicacion = numeroUno * numeroDos;
+
+        //OPERADOR DE DIVISION
+        int division = numeroUno / numeroDos;
+
+        //OPERADOR DE MODULO
+        int modulo = numeroUno % numeroDos;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Resto: " + resto);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        System.out.println("Division: " + division);
+        System.out.println("Modulo: " + modulo);
+    }
+}
